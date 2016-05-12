fcb48196cc5c4c4f5d677f4d46a7bee5e015fddc
Jose Luis <winpcjose@gmail.com>
2016-05-12 01:21:17 -0500

    Modificando Controlador

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 class RecipesController < ApplicationController
   #Segun el enunciado la palabra chocolate esta or defecto
   def index
-  	@param=(params[:search]=="")?("chocolate"):(params[:search]);
+  	@param=(params[:search] or  "chocolate")
   	lista = Recipe.for(@param)
   	@recetas =lista["recipes"]
   end
